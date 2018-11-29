de7a4e05cd4353a027ecd0bffb7571a043b88ab8
kenichi <slow_life_1407@yahoo.co.jp>
2018-11-29 00:00:05 +0000

    edit routes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,13 +5,13 @@ class LikesController < ApplicationController
     post = Post.find(params[:post_id])
     current_user.like(post)
     flash[:success] = 'お気に入りに追加しました。'
-    redirect_to user_path(current_user)
+    redirect_to root_url
   end
 
   def destroy
     post = Post.find(params[:post_id])
     current_user.unlike(post)
     flash[:success] = 'お気に入りから外しました。'
-    redirect_to user_path(current_user)
+    redirect_back(fallback_location: root_path)
   end
 end
